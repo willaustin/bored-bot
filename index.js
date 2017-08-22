5d6a0dd0bacb78eb4f3afba454f00cc65c9c5801
Will Austin <will.austin@joltup.com>
2017-08-22 00:27:55 -0600

    Add in json body parser

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+'use strict';
 const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
